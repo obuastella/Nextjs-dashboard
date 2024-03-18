c526e19ef97a750e6b7a9d920fe37af0826e48bb
Obua Stella <stellaobua123@gmail.com>
2024-03-18 16:16:56 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-center text-orang-400">hello world</h1>
+      <button className="w-full p-2 rounded-lg border-2">Click me!</button>
+    </main>
+  );
+}
